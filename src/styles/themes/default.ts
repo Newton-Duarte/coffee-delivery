11e6d7fc031f0c37d8e127fd0d5fb20680a8df06
Newton Duarte <newton_@hotmail.com>
2024-02-03 09:22:17 -0300

    fix: theme folder name

---
+++ src/styles/themes/default.ts
@@ -0,0 +1,20 @@
+export const defaultTheme = {
+  white: '#fff',
+  yellow: '#DBAC2C',
+  yellowLight: '#F1E9C9',
+  yellowDark: '#C47F17',
+  purple: '#8047F8',
+  purpleDark: '#4B2995',
+  purpleLight: '#EBE5F9',
+
+  baseTitle: '#272221',
+  baseSubtitle: '#403937',
+  baseText: '#574F4D',
+  baseLabel: '#8D8686',
+  baseHover: '#D7D5D5',
+  baseButton: '#E6E5E5',
+  baseInput: '#EDEDED',
+  baseCard: '#F3F2F2',
+
+  bg: '#FAFAFA',
+} as const
