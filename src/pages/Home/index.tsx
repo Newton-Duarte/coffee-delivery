655d4355bfc82a9ad44b694b39fff4b8ebe3f914
Newton Duarte <newton_@hotmail.com>
2024-02-03 10:01:24 -0300

    feat: add home hero section

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,3 +1,45 @@
+import { Coffee, Package, ShoppingCart, Timer } from 'phosphor-react'
+import heroImg from '../../assets/coffee-delivery-hero.png'
+
+import * as S from './styles'
+
 export function Home() {
-  return <div>Home</div>
+  return (
+    <S.Container>
+      <S.Content>
+        <h1>Encontre o café perfeito para qualquer hora do dia</h1>
+        <p>
+          Com o Coffee Delivery você recebe seu café onde estiver, a qualquer
+          hora
+        </p>
+        <S.Info>
+          <S.InfoItem>
+            <div>
+              <ShoppingCart size={16} weight="fill" />
+            </div>
+            <span>Compra simples e segura</span>
+          </S.InfoItem>
+          <S.InfoItem>
+            <div>
+              <Package size={16} weight="fill" />
+            </div>
+            <span>Compra simples e segura</span>
+          </S.InfoItem>
+          <S.InfoItem>
+            <div>
+              <Timer size={16} weight="fill" />
+            </div>
+            <span>Compra simples e segura</span>
+          </S.InfoItem>
+          <S.InfoItem>
+            <div>
+              <Coffee size={16} weight="fill" />
+            </div>
+            <span>Compra simples e segura</span>
+          </S.InfoItem>
+        </S.Info>
+      </S.Content>
+      <img src={heroImg} alt="" />
+    </S.Container>
+  )
 }
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,3 +1,80 @@
 import styled from 'styled-components'
 
-export const Container = styled.div``
+export const Container = styled.main`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  gap: 3.5rem;
+  margin: 5.875rem 0;
+
+  > img {
+    justify-self: end;
+  }
+`
+
+export const Content = styled.div`
+  margin: 0 auto;
+
+  h1 {
+    font:
+      800 3rem 'Baloo 2',
+      sans-serif;
+    line-height: 1.3;
+    max-width: 36.75rem;
+    margin-bottom: 1rem;
+  }
+
+  p {
+    font-size: 0.875rem;
+    max-width: 36.75rem;
+  }
+`
+
+export const Info = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 1.25rem;
+  margin-top: 4rem;
+`
+
+export const InfoItem = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 0.75rem;
+
+  &:nth-child(1) {
+    div {
+      background-color: ${(props) => props.theme.yellowDark};
+    }
+  }
+
+  &:nth-child(2) {
+    div {
+      background-color: ${(props) => props.theme.baseText};
+    }
+  }
+
+  &:nth-child(3) {
+    div {
+      background-color: ${(props) => props.theme.yellow};
+    }
+  }
+
+  &:nth-child(4) {
+    div {
+      background-color: ${(props) => props.theme.purple};
+    }
+  }
+
+  div {
+    width: 2rem;
+    height: 2rem;
+    border-radius: 1rem;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    svg {
+      color: ${(props) => props.theme.white};
+    }
+  }
+`
