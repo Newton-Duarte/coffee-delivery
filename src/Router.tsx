053a718f03c60131ee0dcb33e88777f0a5a003a9
Newton Duarte <newton_@hotmail.com>
2024-02-03 09:22:32 -0300

    feat: add default layout

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,13 +1,16 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { Home } from './pages/Home'
 import { Cart } from './pages/Cart'
+import { DefaultLayout } from './layouts/DefaultLayout'
 
 export function Router() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/cart" element={<Cart />} />
+        <Route path="/" element={<DefaultLayout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/cart" element={<Cart />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   )
